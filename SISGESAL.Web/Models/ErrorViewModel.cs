f663ab65cfe3dafe9c1e2b0221a52470f6ac891b
GERARD FRANCOIS RAMOS MARTINEZ <gerard.ramos@unah.hn>
2025-01-08 15:44:12 -0600

    First commit

---
+++ SISGESAL.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SISGESAL.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
